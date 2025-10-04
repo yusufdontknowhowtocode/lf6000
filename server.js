95175ed355ad483bca696e64ecb0363235cc7a72
Yusuf Kaymak <ykaymakusa@gmail.com>
2025-10-03 22:24:58 -0400

    m

--- server.js
+++ server.js
@@ -69,6 +69,7 @@ const corsOrigins = (process.env.CORS_ORIGIN || '').split(',').map(s=>s.trim()).
 app.use(cors({ origin: corsOrigins.length ? corsOrigins : false, credentials: false }));
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public'), { extensions: ['html'] }));
 app.use('/public', express.static(path.join(__dirname, 'public'), { extensions: ['html'] }));
 
 /* ------------------------------ SSE helper ------------------------------ */
